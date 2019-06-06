5549d9c8f437caebf76eb0a7ffbde971ce48afca
diegorico1999 <43432721+diegorico1999@users.noreply.github.com>
2019-06-06 12:16:38 -0500

    Update 21.hs

--- 21.hs
+++ 21.hs
@@ -1,250 +1,153 @@
-import System.Aleatoria
-import Control.Monad.State
+module BlackJack where
 
-module Mazo 
-  ( Carta
-  , posiblesPuntos
-  , Mazo
-  , MazoS
-  , Cartas
-  , gen
-  , mkMazo
-  , draw
-  , shuffle
-  , tomarAleatoriaCarta
-  , tomarCartaAt ) where
+import Cards
+import Wrapper
+import Test.QuickCheck
+import System.Random
 
-data Carta = Ace
-          | Two
-          | Three
-          | Four
-          | Five
-          | Six
-          | Seven
-          | Eight
-          | Nine
-          | Ten
-          | Jack
-          | Queen
-          | King
-          deriving (Enum, Show)
+-- size hand2
+-- = size (Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty))
+-- = 1 + size (Add (Card Jack Spades) Empty)
+-- = 2 + size Empty
+-- = 2 + 0
+-- = 2
 
-posiblesPuntos :: [Carta] -> [Int]
-posiblesPuntos = go [0]
-  where go n []           = n
-        go ns (Ace:rest)   = go (map ((+) 1) ns ++ map ((+) 11) ns) rest
-        go ns (Two:rest)   = go (map ((+) 2) ns) rest
-        go ns (Three:rest) = go (map ((+) 3) ns) rest
-        go ns (Four:rest)  = go (map ((+) 4) ns) rest
-        go ns (Five:rest)  = go (map ((+) 5) ns) rest
-        go ns (Six:rest)   = go (map ((+) 6) ns) rest
-        go ns (Seven:rest) = go (map ((+) 7) ns) rest
-        go ns (Eight:rest) = go (map ((+) 8) ns) rest
-        go ns (Nine:rest)  = go (map ((+) 9) ns) rest
-        go ns (Ten:rest)   = go (map ((+) 10) ns) rest
-        go ns (Jack:rest)  = go (map ((+) 10) ns) rest
-        go ns (Queen:rest) = go (map ((+) 10) ns) rest
-        go ns (King:rest)  = go (map ((+) 10) ns) rest
+empty :: Hand
+empty = Empty
 
-data Mazo = Mazo
-  { Cartas :: [Carta]
-  , gen :: StdGen }
-  deriving (Show)
+-- Use valueRank to determine the value of the current hand.
+totalValue :: Hand -> Integer
+totalValue Empty                    = 0
+totalValue (Add (Card rank _) hand) = valueRank rank + totalValue hand
 
-mkMazo :: StdGen -> Mazo
-mkMazo g = 
-  Mazo { Cartas = [ Carta | Carta <- [Ace ..], _ <- [1..4] :: [Int] ]
-       , gen = g }
+-- fixes the value of the hand if it is more than 21 and it contains ace/s.
+value :: Hand -> Integer
+value Empty                    = 0
+value hand 
+  | totalValue hand <= 21 = totalValue hand
+  | totalValue hand > 21  = totalValue hand - (10 * numberOfAces hand)
 
-type MazoS a = State Mazo a
+-- Values each rank, returns the value in numbers
+valueRank :: Rank -> Integer
+gameOver hand = value hand > 21
+valueRank King        = 10
+valueRank Queen       = 10
+valueRank Jack        = 10
+valueRank (Numeric x) = x
+valueRank Ace         = 11
 
-draw :: MazoS Carta
-draw = tomarCartaAt 0
+-- Values a Card given the rank.
+valueCard :: Card -> Integer
+valueCard (Card rank _) = valueRank rank 
 
-shuffle :: MazoS ()
-shuffle = do
-  curr <- get
-  shuffled <- replicateM 52 tomarAleatoriaCarta
-  put curr { Cartas = shuffled }
+-- Counts the number of Aces in the current hand.
+numberOfAces :: Hand -> Integer
+numberOfAces Empty = 0
+numberOfAces (Add (Card Ace _) hand) = 1 + numberOfAces hand
+numberOfAces (Add (Card _ _) hand)   = 0 + numberOfAces hand
 
-tomarAleatoriaCarta :: MazoS Carta
-tomarAleatoriaCarta = do
-  curr <- get
-  let n = length $ Cartas curr
-      (i, gen') = AleatoriaR (0, n) $ gen curr
-  Carta <- tomarCartaAt i
-  put curr { gen = gen' }
-  return Carta
+-- If the hand is valued more than 21, gameOver is set to True.
+gameOver :: Hand -> Bool
 
-tomarCartaAt :: Int -> MazoS Carta
-tomarCartaAt i = do
-  curr <- get
-  let (Cartas', Cartas'') = splitAt (i + 1) $ Cartas curr
-      Carta              = last Cartas'
-      newCartas          = init Cartas' ++ Cartas''
-  put curr { Cartas = newCartas }
-  return car
-module Main where
+-- WHO WILL WIN!
+winner :: Hand -> Hand -> Player
+winner guest bank
+  | gameOver guest == True    = Bank
+  | gameOver bank  ==  True   = Guest
+  | value guest > value bank  = Guest 
+  | value guest <= value bank = Bank
 
-import Mazo
+-- Combines 2 different hands. (hand1 <+ hand2 = hand3)
+(<+) :: Hand -> Hand -> Hand
+hand1 <+ Empty          = hand1
+Empty <+ hand2          = hand2
+Add card hand1 <+ hand2 = Add card (hand1 <+ hand2)
 
-import Data.List
-import System.Aleatoria
-import Control.Monad.State
+-- Property, is (<+) associative
+prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
+prop_onTopOf_assoc p1 p2 p3 = p1 <+ (p2 <+ p3) == (p1 <+ p2) <+ p3
+
+-- Property, is the size different
+prop_size_onTopOf :: Hand -> Hand -> Bool
+prop_size_onTopOf p1 p2 = size (p1 <+ p2) == size p1 + size p2
+
+-- Combines all the complete suits into one hand creating a deck of
+-- 52 cards.
+fullDeck :: Hand
+fullDeck = ((completeSuit ranks Hearts   <+
+           completeSuit ranks Spades)    <+
+           (completeSuit ranks Diamonds  <+
+           completeSuit ranks Clubs))
+
+-- List of all the existing ranks.
+ranks :: [Rank]
+ranks =  [Ace, King, Queen, Jack] ++ [Numeric x | x <- [2..10]]
+
+-- Given a suit and a list of ranks (the list of all the ranks above)
+-- makes a complete set of cards for the said suit.
+completeSuit :: [Rank] -> Suit -> Hand
+completeSuit [] suit     = Empty
+completeSuit (x:xs) suit = (Add (Card x suit) Empty) <+ completeSuit xs suit
+
+-- Takes the first card in the deck and places it in the hand.
+draw :: Hand -> Hand -> (Hand, Hand)
+draw Empty hand            = error "draw: The deck is empty."
+draw (Add card deck) hand  = (deck, (Add card hand))
+
+-- Start the bank AI.
+playBank :: Hand -> Hand 
+playBank deck = playBank' deck Empty
+
+-- The bank will continue to draw until its hand is valued 16 or more.
+playBank' :: Hand -> Hand -> Hand
+playBank' deck bankHand | value bankHand < 16   = playBank' deck' bankHand'
+                        | value bankHand >= 16  = bankHand
+  where (deck', bankHand') = draw deck bankHand
+
+-- Given a StdGen and a deck (or hand) it will create a random number between
+-- 0 and the size of the deck/hand.
+randomNum :: StdGen -> Hand -> (Integer, StdGen)
+randomNum gen deck = (randomR(0, (size(deck) - 1)) gen)
+
+-- Start the shuffling! :)
+shuffle :: StdGen -> Hand -> Hand
+shuffle gen deck = shuffle' gen deck Empty -- Empty == shuffled deck
+
+-- Shuffle' is the help function for shuffle. It'll call randomNum and rndCard
+-- to get random cards and numbers and continue to add them into the new deck.
+shuffle' :: StdGen -> Hand -> Hand -> Hand
+shuffle' gen Empty hand = hand <+ Empty
+shuffle' gen deck hand  = shuffle' gen' hand' (Add card' hand)
+   where (rnd', gen')   = randomNum gen deck
+         (hand', card') = rndCard deck Empty rnd'
+
+-- Picks a random card, given from randomNum
+rndCard :: Hand -> Hand -> Integer -> (Hand, Card)
+rndCard (Add card deck) save rnd | rnd == 0 = ((deck <+ save), card)
+                                 | rnd > 0  = rndCard deck (Add card save) (rnd - 1)
+
+prop_shuffle_sameCards :: StdGen -> Card -> Hand -> Bool
+prop_shuffle_sameCards g c h =
+  c `belongsTo` h == c `belongsTo` shuffle g h
+
+belongsTo :: Card -> Hand -> Bool
+c `belongsTo` Empty    = False
+c `belongsTo` Add c' h = c == c' || c `belongsTo` h
+
+prop_size_shuffle :: StdGen -> Hand -> Bool
+prop_size_shuffle g Empty = False
+prop_size_shuffle g h     = size(h) == size(shuffle g h)
+
+implementation = Interface
+  {  iEmpty     = empty
+  ,  iFullDeck  = fullDeck
+  ,  iValue     = value
+  ,  iGameOver  = gameOver
+  ,  iWinner    = winner
+  ,  iDraw      = draw
+  ,  iPlayBank  = playBank
+  ,  iShuffle   = shuffle
+  }
 
 main :: IO ()
-main = do
-  stdGen <- getStdGen
-  evalStateT JuegoLoop $ mkJuego stdGen hiturnlessTwentyOne
-
-data Juego = Juego
-  { Mazo :: Mazo
-  , jugadorMano :: [Carta]
-  , jugadorAccion :: Accion
-  , dealerMano :: [Carta]
-  , dealerAccion :: Accion
-  , dealerEstrategia :: Estrategia }
-
-type JuegoS a = StateT Juego a
-
-data Accion = Hit | Stay deriving (Eq, Read)
-
-type Estrategia = [Carta] -> JuegoS IO (Accion)
-
-
-JuegoLoop :: JuegoS IO ()
-JuegoLoop = do
-  curr <- get
-  when ((jugadorAccion curr) == Hit) ManejarJugador
-  when ((dealerAccion curr) == Hit) ManejarDealer
-  JuegoTerminado <- isJuegoTerminado
-  when JuegoTerminado ManejarJuegoTerminado
-  when (not JuegoTerminado) JuegoLoop
-
-ManejarJugador :: JuegoS IO ()
-ManejarJugador = do
-  curr <- get
-  input <- liftIO $ do
-    let JugadorH = jugadorMano curr
-    putStrLn $ "Tu Mano: " ++ (show JugadorH)
-    putStrLn $ "Mano del dealer: " ++ (showDealer $ dealerMano curr)
-    putStrLn "What do You want to do? (Hit/Stay)"
-    input <- getLine
-    return input
-
-  let Accion = read input :: Accion
-  when (Accion == Hit) $ do
-    let (Carta, Mazo') = runState draw $ Mazo curr
-    put curr { Mazo = Mazo'
-             , jugadorMano = Carta : jugadorMano curr }
-    new <- get
-    let JugadorH = jugadorMano new
-    liftIO . putStrLn $ "Tu Mano: " ++ (showDealer JugadorH)
-
-  when (Accion == Stay) $ do
-    put curr { jugadorAccion = Stay }
-
-ManejarDealer :: JuegoS IO ()
-ManejarDealer = do
-  curr <- get
-  Accion <- dealerEstrategia curr $ dealerMano curr
-  when (Accion == Hit) $ do
-    let (Carta, Mazo') = runState draw $ Mazo curr
-    put curr { Mazo = Mazo'
-             , dealerMano = Carta : dealerMano curr }
-    new <- get
-    let dealerH = dealerMano new
-    liftIO . putStrLn $ "El dealer hit."
-    liftIO . putStrLn $ "Mano del dealer: " ++ (showDealer dealerH)
-
-  when (Accion == Stay) $ do
-    put curr { dealerAccion = Stay }
-    liftIO . putStrLn $ "El dealer se queda."
-
-isJuegoTerminado :: JuegoS IO Bool
-isJuegoTerminado = do
-  curr <- get
-  let JugadorA    = jugadorAccion curr
-      dealerA    = dealerAccion curr
-      JugadorH    = jugadorMano curr
-      dealerH    = dealerMano curr
-      bothStayed = (JugadorA == Stay && dealerA == Stay)
-      JugadorBust = bust JugadorH
-      dealerBust = bust dealerH
-      JuegoTerminado = bothStayed || JugadorBust || dealerBust
-
-  when JugadorBust $ liftIO . putStrLn $ "Tu quebraste!"
-  when dealerBust $ liftIO . putStrLn $ "El dealer quebro!"
-
-  return JuegoTerminado
-
-ManejarJuegoTerminado :: JuegoS IO ()
-ManejarJuegoTerminado = do
-  curr <- get
-  let JugadorH = jugadorMano curr
-      dealerH = dealerMano curr
-      Ganasner  = won JugadorH dealerH
-  liftIO . putStrLn $ "You Mano: " ++ (show JugadorH) ++ ", " ++ (show (posiblesPuntos JugadorH))
-  liftIO . putStrLn $ "Mano del dealer: " ++ (show dealerH) ++ ", " ++ (show (posiblesPuntos dealerH))
-  when Ganasner $ liftIO . putStrLn $ "Tu Ganas!"
-  when (not Ganasner) $ liftIO . putStrLn $ "Tu pierdes..."
-
-won :: [Carta] -> [Carta] -> Bool
-won JugadorH dealerH = JugadorScore > dealerScore
-  where JugadorScore = score JugadorH
-        dealerScore = score dealerH
-
-score :: [Carta] -> Int
-score h 
-  | bust h    = 0
-  | oElrwise = best h
-
-bust :: [Carta] -> Bool
-bust = and . map ((<) 21) . posiblesPuntos
-
-twentyOne :: [Carta] -> Bool
-twentyOne = any ((==) 21) . posiblesPuntos
-
-best :: [Carta] -> Int
-best = maximum . filter ((>=) 21) . posiblesPuntos
-
-showDealer :: [Carta] -> String
-showDealer Mano = "[" ++ (show $ head Mano) ++ "," ++ (intersperse ',' hidden) ++ "]"
-  where n = length $ tail Mano
-        hidden = replicate n '?'
-
-mkJuego :: StdGen -> Estrategia -> Juego
-mkJuego g strat = Juego
-  { Mazo = d' 
-  , jugadorMano = JugadorH
-  , jugadorAccion = Hit
-  , dealerMano = dealerH
-  , dealerAccion = Hit
-  , dealerEstrategia = strat }
-  where d = execState shuffle $ mkMazo g
-        ((JugadorH, dealerH), d') = runState deal $ d
-
-deal :: MazoS ([Carta], [Carta])
-deal = do
-  mine   <- draw
-  Yours  <- draw
-  mine'  <- draw
-  Yours' <- draw
-  let me = [mine, mine']
-      You = [Yours, Yours']
-  return (me, You)
-
-hiturnlessTwentyOne :: Estrategia
-hiturnlessTwentyOne Mano
-  | twentyOne Mano = return Stay
-  | oElrwise      = return Hit
-
-hitSometimes :: Double -> Estrategia
-hitSometimes threshold _ = do
-  curr <- get
-  let Mazo' = Mazo curr
-      (num, gen') = Aleatoria $ gen Mazo'
-  put curr { Mazo = Mazo' { gen = gen' } }
-  if num > threshold
-    then return Hit
-    else return Stay
+main = runGame implementation
